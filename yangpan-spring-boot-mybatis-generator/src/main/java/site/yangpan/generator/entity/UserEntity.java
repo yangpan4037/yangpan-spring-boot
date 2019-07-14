e87b9d2f1d3d1c800237e3041ea9d31232b73f86
yangpan <916780135@qq.com>
2019-07-14 16:11:19 +0800

    整合 Mybatis Generator

---
+++ yangpan-spring-boot-mybatis-generator/src/main/java/site/yangpan/generator/entity/UserEntity.java
@@ -0,0 +1,45 @@
+package site.yangpan.generator.entity;
+
+import java.util.Date;
+
+public class UserEntity {
+    private String userId;
+
+    private String username;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
