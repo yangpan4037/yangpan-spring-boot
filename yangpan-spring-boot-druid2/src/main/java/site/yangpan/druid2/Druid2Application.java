99f10c2434187d3c080e8a1d24998065f0c6d940
yangpan <916780135@qq.com>
2019-08-18 10:36:34 +0800

    整合 Druid2

---
+++ yangpan-spring-boot-druid2/src/main/java/site/yangpan/druid2/Druid2Application.java
@@ -0,0 +1,13 @@
+package site.yangpan.druid2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Druid2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Druid2Application.class, args);
+    }
+
+}
